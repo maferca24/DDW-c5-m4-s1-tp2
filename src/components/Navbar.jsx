f64b4f27a623ee69bd626b353111b7f32ce39f08
MAFERCARRE <mafercarrenio@gmail.com>
2026-08-28 23:32:19 -0300

    modifique Nabar, agregue menú

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+function Navbar() {
+  //defino array con los elementos del menu
+  const links = [
+    { id: 1, label: "Inicio", url: "#hero" },
+    { id: 2, label: "Itinerario", url: "#features" },
+    { id: 3, label: "Contacto", url: "#footer" },
+    { id: 4, label: "Momentos Destacados", url: "#destacados" },
+    { id: 5, label: "Frases y Mensajes", url: "#mensajes" }
+  ];
 
-const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    <header >
+      <nav>
+        {/* Logo */}
+        <div >
+          <span>🇻🇦</span> PapaLeonXVEnArg
+        </div>
+
+        {/* Links generados con .map() */}
+        <ul>
+          {links.map((link) => (
+            <li key={link.id}>
+              <a 
+                href={link.url} 
+                
+              >
+                {link.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
-export default Navbar
+export default Navbar;
+
